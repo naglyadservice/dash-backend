447fb6c66f661f3a1e07346880edc0a66ab4b943
danraponga <danrapongha@gmail.com>
2025-02-12 12:30:05 +0100

    feat: init models with db setup & lint

---
+++ src/models/controllers/__init__.py
@@ -0,0 +1,4 @@
+from .carwash import CarwashСontroller
+from .controller import Controller
+from .vacuum import VacuumController
+from .water_vending import WaterVendingController
---
+++ src/models/transactions/__init__.py
@@ -0,0 +1,4 @@
+from .carwash import CarwashTransaction
+from .transaction import Transaction
+from .vacuum import VacuumTransaction
+from .water_vending import WaterVendingTransaction
