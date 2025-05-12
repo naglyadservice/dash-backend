e9e53a985a3e8a0f36b434649f62f5dd58f085ab
toxazhl <zhltoxa@gmail.com>
2025-05-12 15:42:26 +0300

    fix

--- src/dash/presentation/callbacks_wsm/payment_card_get.py
+++ src/dash/presentation/callbacks_wsm/payment_card_get.py
@@ -3,12 +3,16 @@ from dataclasses import dataclass
 from adaptix import Retort, name_mapping
 from ddtrace.trace import tracer
 from dishka import FromDishka
+from structlog import get_logger
 
 from dash.infrastructure.iot.wsm.client import WsmClient
 from dash.infrastructure.repositories.controller import ControllerRepository
 from dash.infrastructure.repositories.customer import CustomerRepository
 
 from .di_injector import datetime_recipe, inject, parse_paylaad, request_scope
+
+logger = get_logger()
+
 
 retort = Retort()
 
@@ -45,6 +49,11 @@ async def payment_card_get_callback(
     controller = await controller_repository.get_wsm_by_device_id(device_id)
 
     if controller is None:
+        logger.info(
+            "Ignoring payment from controller, controller not found by device_id",
+            device_id=device_id,
+            card_id=data.card_uid,
+        )
         return
 
     customer = await customer_repository.get_by_card_id(
@@ -52,6 +61,11 @@ async def payment_card_get_callback(
     )
 
     if customer is None:
+        logger.info(
+            "Ignoring payment from controller, customer not found by card_id",
+            device_id=device_id,
+            card_id=data.card_uid,
+        )
         return
 
     await wsm_client.respond_payment_cart(
