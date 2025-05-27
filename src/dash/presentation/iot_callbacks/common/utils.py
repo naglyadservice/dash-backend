8c47d7a7c6d718e1197c239bc5134089a7142511
danraponga <danrapongha@gmail.com>
2025-05-27 13:37:00 +0200

    fix dt_native_to_zone_aware func

--- src/dash/presentation/iot_callbacks/common/utils.py
+++ src/dash/presentation/iot_callbacks/common/utils.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta, timezone
 
 def dt_naive_to_zone_aware(dt: datetime | str, tz_offset: int) -> datetime:
     if isinstance(dt, str):
-        dt = datetime.strptime(dt, "%Y-%m-%dT%H:%M:%S")
+        dt = datetime.fromisoformat(dt)
 
     tz = timezone(timedelta(hours=tz_offset))
     return dt.replace(tzinfo=tz)
