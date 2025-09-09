8a1793b5a795b8e3d184c06a113e7e7afa982e4a
danraponga <danrapongha@gmail.com>
2025-09-09 14:50:49 +0200

    add logs to begin, improve dt handling

--- src/dash/presentation/iot_callbacks/begin.py
+++ src/dash/presentation/iot_callbacks/begin.py
@@ -1,12 +1,13 @@
-from datetime import UTC, datetime
+from datetime import datetime
 from typing import Any
 
 from dishka import FromDishka
+from structlog import dev, get_logger
 
 from dash.infrastructure.repositories.controller import ControllerRepository
-from dash.main.config import AppConfig
 from dash.presentation.iot_callbacks.common.di_injector import inject, request_scope
 
+logger = get_logger()
 
 @request_scope
 @inject
@@ -14,16 +15,12 @@ async def begin_callback(
     device_id: str,
     data: dict[str, Any],
     controller_repository: FromDishka[ControllerRepository],
-    config: FromDishka[AppConfig],
 ) -> None:
+    logger.info("begin received", device_id=device_id)
     controller = await controller_repository.get_by_device_id(device_id)
 
     if controller is None:
         return
 
-    controller.last_reboot = (
-        datetime.strptime(data["time"], "%d.%m.%YT%H:%M:%S")
-        .astimezone(config.timezone)
-        .astimezone(UTC)
-    )
+    controller.last_reboot = datetime.fromisoformat(data["time"])
     await controller_repository.commit()
